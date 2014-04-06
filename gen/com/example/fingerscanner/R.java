d45cc2779c7dbc31d14f218674a5e0214ed0d70b
haiqnguyen318 <hai@umass.edu>
2014-04-06 00:04:42 -0400

    Bug in onActivityResult

--- gen/com/example/fingerscanner/R.java
+++ gen/com/example/fingerscanner/R.java
@@ -29,17 +29,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000d;
+        public static final int action_settings=0x7f08000e;
         public static final int card=0x7f080004;
         public static final int cardinput=0x7f080005;
-        public static final int choosephoto=0x7f08000a;
+        public static final int choosephoto=0x7f08000b;
         public static final int dob=0x7f080008;
         public static final int dobinput=0x7f080009;
         public static final int name=0x7f080002;
         public static final int nameinput=0x7f080003;
+        public static final int photo=0x7f08000a;
         public static final int register=0x7f080000;
-        public static final int registrationback=0x7f08000b;
-        public static final int registrationsubmit=0x7f08000c;
+        public static final int registrationback=0x7f08000c;
+        public static final int registrationsubmit=0x7f08000d;
         public static final int retrieve=0x7f080001;
         public static final int zipcode=0x7f080006;
         public static final int zipcodeinput=0x7f080007;
