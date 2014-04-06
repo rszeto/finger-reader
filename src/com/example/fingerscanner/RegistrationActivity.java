fcef5b137ed340451b4793ca92b09dce90cabe54
tungpham31 <tungpham31@gmail.com>
2014-04-06 00:35:09 -0400

    Get returned image from intent.
    Signed-off-by: tungpham31 <tungpham31@gmail.com>

--- src/com/example/fingerscanner/RegistrationActivity.java
+++ src/com/example/fingerscanner/RegistrationActivity.java
@@ -1,20 +1,17 @@
 package com.example.fingerscanner;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,10 +21,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.os.Build;
-import android.provider.MediaStore;
 
-public class RegistrationActivity extends Activity implements OnClickListener{
+public class RegistrationActivity extends Activity implements OnClickListener {
 
 	Button photoSelect;
 	Button backButton;
@@ -37,95 +32,95 @@ public class RegistrationActivity extends Activity implements OnClickListener{
 	EditText zipcode;
 	EditText dateOfBirth;
 	ImageView photo;
-	
+
 	public static final String PREF_NAME = "com.example.fingerscanner";
 	private static final int SELECT_PHOTO = 100;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.registration);
-		
+
 		photo = (ImageView) findViewById(R.id.photo);
 		photoSelect = (Button) findViewById(R.id.choosephoto);
 		photoSelect.setOnClickListener(this);
-		
+
 		backButton = (Button) findViewById(R.id.registrationback);
 		backButton.setOnClickListener(this);
-		
+
 		submitButton = (Button) findViewById(R.id.registrationsubmit);
 		submitButton.setOnClickListener(this);
-		
+
 		nameInput = (EditText) findViewById(R.id.nameinput);
 		cardInput = (EditText) findViewById(R.id.cardinput);
 		zipcode = (EditText) findViewById(R.id.zipcodeinput);
 		dateOfBirth = (EditText) findViewById(R.id.dobinput);
-		
+
 	}
-	
-	public void onClick(View v){
-		int id= v.getId();
-		if (id==backButton.getId()){
-			Intent registrationToHome = new Intent(RegistrationActivity.this,MainActivity.class);
+
+	public void onClick(View v) {
+		int id = v.getId();
+		if (id == backButton.getId()) {
+			Intent registrationToHome = new Intent(RegistrationActivity.this,
+					MainActivity.class);
 			RegistrationActivity.this.startActivity(registrationToHome);
 		}
-				
-		else if(id==submitButton.getId()){
-			
-			//retrieve info from registration screen
+
+		else if (id == submitButton.getId()) {
+
+			// retrieve info from registration screen
 			String name = nameInput.getText().toString();
 			String card = cardInput.getText().toString();
 			String zip = zipcode.getText().toString();
 			String birth = dateOfBirth.getText().toString();
-			
-			SharedPreferences prefs = getSharedPreferences(PREF_NAME,Context.MODE_PRIVATE);
+
+			SharedPreferences prefs = getSharedPreferences(PREF_NAME,
+					Context.MODE_PRIVATE);
 			Editor editor = prefs.edit();
-			
-			//get counter, return 0 if there is no counter
-			int ct = prefs.getInt("counter",0);
-			
-			//add info to database
-			editor.putString("name"+ct,name);
-			editor.putString("card"+ct,card);
-			editor.putString("zip"+ct, zip);
-			editor.putString("dob"+ct, birth);
-			
-			//increment counter
+
+			// get counter, return 0 if there is no counter
+			int ct = prefs.getInt("counter", 0);
+
+			// add info to database
+			editor.putString("name" + ct, name);
+			editor.putString("card" + ct, card);
+			editor.putString("zip" + ct, zip);
+			editor.putString("dob" + ct, birth);
+
+			// increment counter
 			ct++;
-			editor.putInt("counter",ct);
-			
+			editor.putInt("counter", ct);
+
 			editor.commit();
-			
-			Intent backToHome = new Intent(RegistrationActivity.this,MainActivity.class);
+
+			Intent backToHome = new Intent(RegistrationActivity.this,
+					MainActivity.class);
 			RegistrationActivity.this.startActivity(backToHome);
 		}
 
-		else if(id==photoSelect.getId()){
-			Intent photoPickerIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-			startActivityForResult(photoPickerIntent, SELECT_PHOTO); 
+		else if (id == photoSelect.getId()) {
+			Intent photoPickerIntent = new Intent(
+					MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(photoPickerIntent, SELECT_PHOTO);
 		}
 	}
 
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) { 
-	    super.onActivityResult(requestCode, resultCode, imageReturnedIntent); 
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent imageReturnedIntent) {
+		super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
 
-	    switch(requestCode) { 
-	    case SELECT_PHOTO:
-	        if(resultCode == RESULT_OK){  
-	            Uri selectedImage = imageReturnedIntent.getData();
-	            try{
-	            	InputStream imageStream = getContentResolver().openInputStream(selectedImage);
-	            	Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
-	        	    photo.setImageBitmap(yourSelectedImage);
-	            }
-	            catch(IOException e){e.printStackTrace();}
-	           
-	        }
-	    }
-	    
+		switch (requestCode) {
+		case SELECT_PHOTO:
+			if (resultCode == RESULT_OK) {
+				Bundle extras = imageReturnedIntent.getExtras();
+				Bitmap imageBitmap = (Bitmap) extras.get("data");
+				photo.setImageBitmap(imageBitmap);
+			}
+		}
+
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -157,8 +152,8 @@ public class RegistrationActivity extends Activity implements OnClickListener{
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.registration,
-					container, false);
+			View rootView = inflater.inflate(R.layout.registration, container,
+					false);
 			return rootView;
 		}
 	}
