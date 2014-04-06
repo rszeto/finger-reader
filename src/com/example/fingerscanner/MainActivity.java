1701797661bb99f8aa621c1aeae6fa271ac3270e
Ryan Szeto <rszeto@umass.edu>
2014-04-06 01:31:38 -0400

    Added handling the scanner when starting, resuming, pausing

--- src/com/example/fingerscanner/MainActivity.java
+++ src/com/example/fingerscanner/MainActivity.java
@@ -1,18 +1,46 @@
 package com.example.fingerscanner;
 
-import SecuGen.FDxSDKPro.JSGFPLib;
-
+import SecuGen.FDxSDKPro.*;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener {
+	
+	private static final String TAG = "SecuGen USB";
+	
 	Button regButton;
 	Button retrieveButton;
+
+	private PendingIntent mPermissionIntent;
+
+	private JSGFPLib sgfplib;
+
+	private boolean mLed;
+
+	private int mImageWidth;
+
+	private int mImageHeight;
+
+	private int[] mMaxTemplateSize;
+
+	//private byte[] mRegisterTemplate;
+
+	//private byte[] mVerifyTemplate;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +50,89 @@ public class MainActivity extends Activity implements OnClickListener{
         regButton.setOnClickListener(this);
         retrieveButton = (Button) findViewById(R.id.retrieve);
         retrieveButton.setOnClickListener(this);
+        
+        mMaxTemplateSize = new int[1];
+        
+        //USB Permissions
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+       	IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+       	registerReceiver(mUsbReceiver, filter);
+        sgfplib = new JSGFPLib((UsbManager)getSystemService(Context.USB_SERVICE));
+		debugMessage("jnisgfplib version: " + sgfplib.Version() + "\n");
+		mLed = false;		
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        long error = sgfplib.Init( SGFDxDeviceName.SG_DEV_AUTO);
+        if (error != SGFDxErrorCode.SGFDX_ERROR_NONE){
+        	AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+        	if (error == SGFDxErrorCode.SGFDX_ERROR_DEVICE_NOT_FOUND)
+        		dlgAlert.setMessage("The attached fingerprint device is not supported on Android");
+        	else
+        		dlgAlert.setMessage("Fingerprint device initialization failed!");
+        	dlgAlert.setTitle("SecuGen Fingerprint SDK");
+        	dlgAlert.setPositiveButton("OK",
+        			new DialogInterface.OnClickListener() {
+        		      public void onClick(DialogInterface dialog,int whichButton){
+        		        	finish();
+        		        	return;        		    	  
+        		      }        			
+        			}
+        	);
+        	dlgAlert.setCancelable(false);
+        	dlgAlert.create().show();        	
+        }
+        else {
+	        UsbDevice usbDevice = sgfplib.GetUsbDevice();
+	        if (usbDevice == null){
+	        	AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+	        	dlgAlert.setMessage("SDU04P or SDU03P fingerprint sensor not found!");
+	        	dlgAlert.setTitle("SecuGen Fingerprint SDK");
+	        	dlgAlert.setPositiveButton("OK",
+	        			new DialogInterface.OnClickListener() {
+	        		      public void onClick(DialogInterface dialog,int whichButton){
+	        		        	finish();
+	        		        	return;        		    	  
+	        		      }        			
+	        			}
+	        	);
+	        	dlgAlert.setCancelable(false);
+	        	dlgAlert.create().show();
+	        }
+	        else {
+		        sgfplib.GetUsbManager().requestPermission(usbDevice, mPermissionIntent);
+		        error = sgfplib.OpenDevice(0);
+				debugMessage("OpenDevice() ret: " + error + "\n");
+		        SecuGen.FDxSDKPro.SGDeviceInfoParam deviceInfo = new SecuGen.FDxSDKPro.SGDeviceInfoParam();
+		        error = sgfplib.GetDeviceInfo(deviceInfo);
+				debugMessage("GetDeviceInfo() ret: " + error + "\n");
+		    	mImageWidth = deviceInfo.imageWidth;
+		    	mImageHeight= deviceInfo.imageHeight;
+		        sgfplib.SetTemplateFormat(SGFDxTemplateFormat.TEMPLATE_FORMAT_SG400);
+				sgfplib.GetMaxTemplateSize(mMaxTemplateSize);
+				debugMessage("TEMPLATE_FORMAT_SG400 SIZE: " + mMaxTemplateSize[0] + "\n");
+//		        boolean smartCaptureEnabled = this.mCheckBoxSCEnabled.isChecked();
+//		        if (smartCaptureEnabled)
+		        	sgfplib.writeData((byte)5, (byte)1);
+//		        else
+//		        	sgfplib.writeData((byte)5, (byte)0);
+	        }
+        }
+    }
+    
+    @Override
+    public void onPause() {
+    	sgfplib.CloseDevice();
+        super.onPause();
+    }
+    
+    @Override
+    public void onDestroy() {
+    	sgfplib.CloseDevice();
+    	sgfplib.Close();
+        super.onDestroy();
     }
 
     public void onClick(View v){
@@ -45,4 +156,33 @@ public class MainActivity extends Activity implements OnClickListener{
         return true;
     }
     
+    private void debugMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+    
+  //This broadcast receiver is necessary to get user permissions to access the attached USB device
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+    	public void onReceive(Context context, Intent intent) {
+    		String action = intent.getAction();
+    		//DEBUG Log.d(TAG,"Enter mUsbReceiver.onReceive()");
+    		if (ACTION_USB_PERMISSION.equals(action)) {
+    			synchronized (this) {
+    				UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+    				if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+    					if(device != null){
+    						//DEBUG Log.d(TAG, "Vendor ID : " + device.getVendorId() + "\n");
+    						//DEBUG Log.d(TAG, "Product ID: " + device.getProductId() + "\n");
+    						debugMessage("Vendor ID : " + device.getVendorId() + "\n");
+    						debugMessage("Product ID: " + device.getProductId() + "\n");
+    					}
+    					else
+        					Log.e(TAG, "mUsbReceiver.onReceive() Device is null");    						
+    				} 
+    				else
+    					Log.e(TAG, "mUsbReceiver.onReceive() permission denied for device " + device);    				
+    			}
+    		}
+    	}
+    };  
 }
